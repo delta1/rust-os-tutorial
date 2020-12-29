e3fa879155f08b704249ed34571db4e2e375bc1e
Byron Hambly <byron@hambly.dev>
2020-12-29 11:33:41 +0200

    https://os.phil-opp.com/minimal-rust-kernel/

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
-#![no_std]
-#![no_main]
+// src/main.rs
+
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
 
+/// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"what the fuck is up, world?!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
